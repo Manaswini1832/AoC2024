6c8091b17fda50482ef33ed4864de5fbb3690451
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2024-12-17 00:10:19 +0530

    bug fix+add : call reconstruct path with correct prevDir for each direction of E, reconstructDirs func to make sure no 180deg turns

--- day16_2.cpp
+++ day16_2.cpp
@@ -152,30 +152,65 @@ void printPrevious(vector<vector<vector<tuple<int, int, int>>>> &previous)
     }
 }
 
+// return a vector of possible directions you can backtrack in, based on the direction you're in rn
+vector<int> reconstructionDirections(char initDir)
+{
+    // can't make 180deg turns
+    vector<int> directions;
+    switch (initDir)
+    {
+    case 0: // initDir = top
+        directions.push_back(0);
+        directions.push_back(1);
+        directions.push_back(3);
+        break;
+    case 1: // right
+        directions.push_back(0);
+        directions.push_back(1);
+        directions.push_back(2);
+        break;
+    case 2: // down
+        directions.push_back(1);
+        directions.push_back(2);
+        directions.push_back(3);
+        break;
+    case 3: // left
+        directions.push_back(0);
+        directions.push_back(2);
+        directions.push_back(3);
+        break;
+    default:
+        break;
+    }
+    return directions;
+}
+
 // this basically is a backtracking function that does a dfs on all possible paths from E to S and checks if cost of that path = ref cost or not
 void reconstructPath(vector<vector<vector<tuple<int, int, int>>>> &previous,
                      pair<int, int> startPos,
                      pair<int, int> endPos,
                      set<pair<int, int>> &pathNodes,
                      set<pair<int, int>> &temppathNodes,
-                     vector<vector<bool>> &visited, int referenceCost, int currentPathCost, int prevDir)
+                     vector<vector<vector<bool>>> &visited, int referenceCost, int currentPathCost, int prevDir)
 {
+    if (currentPathCost > referenceCost)
+        return;
     if (startPos == endPos)
     {
         // add temppathnodes to pathnodes only if cost of path from E to here was same as referenceCost
         if (currentPathCost != referenceCost)
             return;
-        for (auto node : temppathNodes)
-        {
-            pathNodes.insert(node);
-        }
+        pathNodes.insert(temppathNodes.begin(), temppathNodes.end());
         return;
     }
 
     int currX = endPos.first, currY = endPos.second;
-    visited[currX][currY] = true;
 
-    for (int dir = 0; dir < 4; dir++)
+    visited[currX][currY][prevDir] = true;
+
+    vector<int> reconstructDirs = reconstructionDirections(prevDir);
+
+    for (auto dir : reconstructDirs)
     {
         auto prevTuple = previous[currX][currY][dir];
         int prevX = get<0>(prevTuple), prevY = get<1>(prevTuple);
@@ -184,20 +219,12 @@ void reconstructPath(vector<vector<vector<tuple<int, int, int>>>> &previous,
         {
             temppathNodes.insert({currX, currY});
             // dfs from E to S
-            if (!visited[prevX][prevY])
+            if (!visited[prevX][prevY][dir])
             {
-                if (prevDir != -1)
-                {
-                    if (prevDir == dir)
-                        reconstructPath(previous, startPos, {prevX, prevY}, pathNodes, temppathNodes, visited, referenceCost, currentPathCost + 1, dir);
-                    else
-                        reconstructPath(previous, startPos, {prevX, prevY}, pathNodes, temppathNodes, visited, referenceCost, currentPathCost + 1 + 1000, dir);
-                }
-                else
-                {
+                if (prevDir == dir)
                     reconstructPath(previous, startPos, {prevX, prevY}, pathNodes, temppathNodes, visited, referenceCost, currentPathCost + 1, dir);
+                else
                     reconstructPath(previous, startPos, {prevX, prevY}, pathNodes, temppathNodes, visited, referenceCost, currentPathCost + 1 + 1000, dir);
-                }
             }
             // remove node if it doesn't contribute to a valid path
             if (pathNodes.find({currX, currY}) == pathNodes.end())
@@ -207,7 +234,7 @@ void reconstructPath(vector<vector<vector<tuple<int, int, int>>>> &previous,
         }
     }
 
-    visited[currX][currY] = false;
+    visited[currX][currY][prevDir] = false;
 }
 
 int main()
@@ -239,18 +266,23 @@ int main()
         }
     }
 
+    cout << "rows : " << grid.size() << ", cols : " << grid[0].size() << endl;
+
     vector<vector<vector<tuple<int, int, int>>>> previous(
         grid.size(),
         vector<vector<tuple<int, int, int>>>(grid[0].size(),
                                              vector<tuple<int, int, int>>(4, {-1, -1, -1})));
     int referenceCost = dijkstra(grid, startPos, endPos, previous);
+    cout << "Reference cost : " << referenceCost << endl;
     // printPrevious(previous);
 
     set<pair<int, int>> pathNodes;
     pathNodes.insert(startPos);
     set<pair<int, int>> temppathNodes;
-    vector<vector<bool>> visited(r, vector<bool>(c, false));
-    reconstructPath(previous, startPos, endPos, pathNodes, temppathNodes, visited, referenceCost, 0, -1);
+    vector<vector<vector<bool>>> visited(r, vector<vector<bool>>(c, vector<bool>(4, false)));
+
+    for (int dir = 0; dir < 4; dir++)
+        reconstructPath(previous, startPos, endPos, pathNodes, temppathNodes, visited, referenceCost, 0, dir);
 
     for (const auto &node : pathNodes)
     {
